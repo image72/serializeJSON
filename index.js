975a10af8507aa107e2042720a927ef76a20a2c1
image72 <image72@msn.com>
2016-08-10 19:56:35 +0800

    add test file and detect multiple select.

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 module.export = serializeJSON
 
-function serializeJSON(form) {
-  var json = {};
-  [].forEach.call(form.querySelectorAll('input,select,textarea'), function(el) {
-    var key = el.name,
-      val = el.value;
-    //var validElem = val && el.hasAttribute('name') && !el.disabled;
-    var validElem = val != '' && val !== undefined && val !== null && el.hasAttribute('name') && !el.disabled;
-    if (validElem) {
-      if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'checkbox') {
-        el.checked && ( (Object.prototype.toString.call(json[key]) == '[object Array]' ) ? json[key].push(val) : json[key] = [val]);
-      } else if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'radio') {
-        el.checked && (json[key] = val);
-      } else {
-        json[key] = val;
+  function serializeJSON(form) {
+    var json = {};
+    [].forEach.call(form.querySelectorAll('input,select,textarea'), function(el) {
+      var key = el.name,
+        val = el.value;
+      //var validElem = val && el.hasAttribute('name') && !el.disabled;
+      var validElem = val != '' && val !== undefined && val !== null && el.hasAttribute('name') && !el.disabled;
+      if (validElem) {
+        if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'checkbox') {
+          el.checked && ( (Object.prototype.toString.call(json[key]) == '[object Array]' ) ? json[key].push(val) : json[key] = [val]);
+        } else if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'radio') {
+          el.checked && (json[key] = val);
+        } else if (el.nodeName.toLowerCase() == 'select') {
+          if (el.multiple) {
+            [].forEach.call(el.selectedOptions, function(ele) {
+              ele.checked && (json[key] = val);
+            })
+          }
+
+        } else {
+          json[key] = val;
+        }
       }
-    }
-  });
-  return json;
-};
+    });
+    return json;
+  };
