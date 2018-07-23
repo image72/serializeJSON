03252c712a000e7c70c3ec793f78319e778ea0f7
image72 <image72@msn.com>
2018-07-23 16:03:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 module.export = serializeJSON
 
-  function serializeJSON(form) {
-    var json = {};
-    [].forEach.call(form.querySelectorAll('input,select,textarea'), function(el) {
-      var key = el.name,
-        val = el.value;
-      //var validElem = val && el.hasAttribute('name') && !el.disabled;
-      var validElem = val != '' && val !== undefined && val !== null && el.hasAttribute('name') && !el.disabled;
-      if (validElem) {
-        if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'checkbox') {
-          el.checked && ( (Object.prototype.toString.call(json[key]) == '[object Array]' ) ? json[key].push(val) : json[key] = [val]);
-        } else if (el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == 'radio') {
-          el.checked && (json[key] = val);
-        } else if (el.nodeName.toLowerCase() == 'select' && el.multiple) {
-          [].forEach.call(el.selectedOptions, function(ele) {
-            ( (Object.prototype.toString.call(json[key]) == '[object Array]' ) ? json[key].push(ele.value) : json[key] = [ele.value]);
-          })
-        } else {
-          json[key] = val;
-        }
+function eleType(el, type) {
+  return el.hasAttribute('type') && el.getAttribute('type').toLowerCase() == type;
+}
+
+function isType(obj, name) {
+  return Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, '$1').toLowerCase() == name;
+}
+
+function serializeJSON(form) {
+  var json = {};
+  [].forEach.call(form.querySelectorAll('input,select,textarea'), function (el) {
+    var key = el.name,
+      val = el.value;
+    //var validElem = val && el.hasAttribute('name') && !el.disabled;
+    var validElem = val != '' && val !== undefined && val !== null && el.hasAttribute('name') && !el.disabled;
+    if (validElem) {
+      if (eleType(el, 'checkbox')) {
+        el.checked && (isType(json[key], 'array') ? json[key].push(val) : json[key] = [val]);
+      } else if (eleType(el, 'radio')) {
+        el.checked && (json[key] = val);
+      } else if (el.nodeName.toLowerCase() == 'select' && el.multiple) {
+        [].forEach.call(el.selectedOptions, function (ele) {
+          (isType(json[key], 'array') ? json[key].push(ele.value) : json[key] = [ele.value]);
+        })
+      } else {
+        json[key] = val;
       }
-    });
-    return json;
-  };
+    }
+  });
+  return json;
+};
